1b0c083e370853fd1597d284ce5483903aea88d3
Katniss-Nataliia <natko.boiko@gmail.com>
2023-05-28 13:11:56 -0600

    coded

--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".feedback-form");
+const email = document.querySelector("input");
+const message = document.querySelector("textarea");
+const LOCAL_STORAGE_KEY = "feedback-form-state";
 
+updateOutput()
+form.addEventListener("submit", saveMessage);
+
+function saveMessage (){
+    localStorage.setItem(LOCAL_STORAGE_KEY, form.email.value);
+    localStorage.getItem(LOCAL_STORAGE_KEY, form.message.value)
+    form.reset();
+}
+
+function updateOutput(){
+   const savedText = localStorage.getItem(LOCAL_STORAGE_KEY);
+   if(!savedText){
+    return
+   }
+   updateOutput(savedText)
+}
