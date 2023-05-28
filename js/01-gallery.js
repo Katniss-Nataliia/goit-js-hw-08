94538859946686e4dc56fb5369487792f9a19f9b
Katniss-Nataliia <natko.boiko@gmail.com>
2023-05-27 20:53:22 -0600

    added some code

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+// import SimpleLightbox from "simplelightbox";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const createMarkUpEl = createMarkUp(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", createMarkUpEl)
+
+function createMarkUp(images){
+    return images.map((item)=>
+    `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${item.preview}" onclick = "event.preventDefault()">
+      <img class="gallery__image" src="${item.original}" alt="${item.description}" />
+   </a>
+    </li>
+    `).join("")
+}
+
+new SimpleLightbox(".gallery a", {
+    captionSelector:"img",
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: "bottom",
+})
--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,34 @@
 
+// import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.getVideoTitle().then(function(title){
+    console.log('title: ', title)
+})
+
+
+const onPlay = function(data){
+    console.log('played the video')
+}
+player.on('play', onPlay);
+
+const saveData = function (){
+    localStorage.setItem(LOCALSTORAGE_KEY, iframe.currentTime);
+}
+player.on('timeupdate', saveData);
+
+player.setCurrentTime().then(function(seconds){
+    localStorage.getItem(LOCALSTORAGE_KEY, iframe.seconds)
+}).catch(function(error){
+    switch(error.name){
+        case 'RangeError':
+            "time was 0";
+            break;
+        default:
+            "error occured";
+            break;
+    }
+})
