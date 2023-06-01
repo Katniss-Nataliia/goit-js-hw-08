48640f41cd302ee51be6ffa44c8a408b5175eca4
Katniss-Nataliia <natko.boiko@gmail.com>
2023-05-31 20:50:24 -0600

    coded to the end

--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,20 +1,37 @@
 const form = document.querySelector(".feedback-form");
-const email = document.querySelector("input");
-const message = document.querySelector("textarea");
+
 const LOCAL_STORAGE_KEY = "feedback-form-state";
 
-updateOutput()
-form.addEventListener("submit", saveMessage);
+let data = {}
 
-function saveMessage (){
-    localStorage.setItem(LOCAL_STORAGE_KEY, form.удуьутеі.value);
-    form.reset();
+const collectData = () => {
+    try{
+        let formData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        if(!formData){
+            return
+        }
+        data = formData;
+        form.email.value = data.email;
+        form.message.value = data.message;
+    }
+    catch (error){
+        console.log(error.message);
+    }
 }
 
-function updateOutput(){
-   const savedText = localStorage.getItem(LOCAL_STORAGE_KEY);
-   if(!savedText){
-    return
-   }
-   updateOutput(savedText)
+const autofillData = (event) => {
+    data = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    data[event.target.value] = event.target.value;
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data))
 }
+
+const onFormSubmit = (event) => {
+    event.preventDefault();
+    event.target.reset;
+    localStorage.removeItem(LOCAL_STORAGE_KEY);
+
+}
+
+collectData();
+
+form.addEventListener('submit', onFormSubmit)
